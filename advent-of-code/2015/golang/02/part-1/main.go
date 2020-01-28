573656f016ad578df86dfa7dd13fda3edafd44ab
Larry Botha <larry@fixate.it>
2020-01-27 22:59:46 -0500

    begin problem

--- advent-of-code/2015/golang/02/part-1/main.go
+++ advent-of-code/2015/golang/02/part-1/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+)
 
+func handleErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	filename := os.Args[1]
+	wd, err := os.Getwd()
+	paths := []string{wd, filename}
+
+	handleErr(err)
+
+	absFilePath := path.Join(paths...)
+
+	data, err := ioutil.ReadFile(absFilePath)
+
+	handleErr(err)
+
+	fmt.Printf("filename: %s\n", data)
+
+	// split text by newline
+	// split each line by x
+	// create combinations of multiple
+	// add half of the smallest
+	// sum everything
 }
