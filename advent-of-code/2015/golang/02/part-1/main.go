20f10cbd3385232e20395bba99050188425074ac
Larry Botha <larry@fixate.it>
2020-01-28 23:59:24 -0500

    WIP - get areas

--- advent-of-code/2015/golang/02/part-1/main.go
+++ advent-of-code/2015/golang/02/part-1/main.go
@@ -6,12 +6,42 @@ import (
 	"log"
 	"os"
 	"path"
+	"regexp"
+	"strconv"
 )
 
 func handleErr(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getDimStrings(str []byte) [][]byte {
+	r, _ := regexp.Compile(`\w+`)
+	dims := r.FindAll(str, -1)
+
+	return dims
+}
+
+func getDims(str []byte) (int, int, int) {
+	r, _ := regexp.Compile(`\d+`)
+	dims := r.FindAll(str, -1)
+	intDims := make([]int, 3)
+
+	for i, v := range dims {
+		intDim, _ := strconv.Atoi(string(v))
+		intDims[i] = intDim
+	}
+
+	return intDims[0], intDims[1], intDims[2]
+}
+
+func getArea(dimsString []byte) int {
+	b, w, h := getDims(dimsString)
+
+	fmt.Printf("b: %d, w: %d, h: %d\n", b, w, h)
+
+	return b * w
 }
 
 func main() {
@@ -22,12 +52,15 @@ func main() {
 	handleErr(err)
 
 	absFilePath := path.Join(paths...)
-
 	data, err := ioutil.ReadFile(absFilePath)
 
 	handleErr(err)
 
-	fmt.Printf("filename: %s\n", data)
+	dimStrings := getDimStrings(data)
+
+	for _, dimString := range dimStrings {
+		getArea(dimString)
+	}
 
 	// split text by newline
 	// split each line by x
